15ed5963ea4091f8792605e408cd1a5e708f384a
DChaney <dominic.chaney@yahoo.com>
2024-09-06 14:56:39 -0500

    Converted index.html to index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php 
+    $title = 'Flixx';
+    include_once './header.php';
+?>
+
+    <!-- Now Playing Section -->
+    <section class="now-playing">
+      <h2>Now Playing</h2>
+      <div class="swiper">
+        <div class="swiper-wrapper">
+          <!-- <div class="swiper-slide">
+            <a href="movie-details.html?id=1">
+              <img src="./images/no-image.jpg" alt="Movie Title" />
+            </a>
+            <h4 class="swiper-rating">
+              <i class="fas fa-star text-secondary"></i> 8 / 10
+            </h4>
+          </div> -->
+        </div>
+      </div>
+    </section>
+
+    <!-- Search Movies -->
+    <?php include_once './components/searchbox.php'; ?>
+
+    <!-- Popular Movies -->
+    <section class="container">
+      <h2>Popular Movies</h2>
+      <div id="popular-movies" class="grid"></div>
+    </section>
+
+    <!-- Footer Section -->
+     <?php include_once './footer.php'; ?>
+
+    <!-- Shows a spinner when the page is loading -->
+    <?php include_once './components/spinner.php' ?>
+  </body>
+</html>
