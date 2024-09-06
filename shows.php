36b5f76fea7e17dbaaa1784e872c4847887a908c
DChaney <dominic.chaney@yahoo.com>
2024-09-06 15:07:15 -0500

    Converted show.html to .php, added includes

---
+++ shows.php
@@ -0,0 +1,16 @@
+<?php $title = 'Popular Shows';
+include_once './header.php'; ?>
+
+    <!-- Popular TV Shows -->
+    <section class="container">
+      <h2>Popular TV Shows</h2>
+      <div id="popular-shows" class="grid"></div>
+    </section>
+
+    <!-- Footer Section -->
+   <?php include_once './footer.php' ?>
+
+    <!-- Shows a spinner when the page is loading -->
+    <?php include_once './components/spinner.php' ?>
+  </body>
+</html>
