88cfef2e8abd90f897ba9b529553895532b77d18
DChaney <dominic.chaney@yahoo.com>
2024-09-18 13:29:26 -0500

    code prettier

--- components/searchbox.php
+++ components/searchbox.php
@@ -1,25 +1,25 @@
 <section class="search">
     <div class="container">
-    <div id="alert"></div>
-    <form action="/search.php" class="search-form">
-        <!-- movies and shows radio box -->
-        <div class="search-radio">
-        <input type="radio" id="movie" name="type" value="movie" checked />
-        <label for="movies">Movies</label>
-        <input type="radio" id="tv" name="type" value="tv" />
-        <label for="shows">TV Shows</label>
-        </div>
-        <div class="search-flex">
-        <input
-            type="text"
-            name="search-term"
-            id="search-term"
-            placeholder="Enter search term"
-        />
-        <button class="btn" type="submit">
-            <i class="fas fa-search"></i>
-        </button>
-        </div>
-    </form>
+        <div id="alert"></div>
+        <form action="/search.php" class="search-form">
+            <!-- movies and shows radio box -->
+            <div class="search-radio">
+                <input type="radio" id="movie" name="type" value="movie" checked />
+                <label for="movies">Movies</label>
+                <input type="radio" id="tv" name="type" value="tv" />
+                <label for="shows">TV Shows</label>
+            </div>
+            <div class="search-flex">
+                <input
+                    type="text"
+                    name="search-term"
+                    id="search-term"
+                    placeholder="Enter search term"
+                />
+                <button class="btn" type="submit">
+                    <i class="fas fa-search"></i>
+                </button>
+            </div>
+        </form>
     </div>
 </section>
