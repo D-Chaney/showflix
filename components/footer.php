e41aff418011caba8a896186aa13afe688b80e91
DChaney <dominic.chaney@yahoo.com>
2024-09-18 20:59:08 -0500

    Removed footer socials and added  Copyright stamp instead.

--- components/footer.php
+++ components/footer.php
@@ -2,10 +2,11 @@
 <footer class="main-footer">
     <div class="container">
         <div><span class="logo">SHOWFLIX</span></div>
-        <div class="social-links">
+        <div class="copyright">Copyright &copy; 2024 ShowFlix</div>
+        <!-- <div class="social-links">
             <a href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook-f"></i></a>
             <a href="https://www.twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
             <a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
-        </div>
+        </div> -->
     </div>
 </footer>
